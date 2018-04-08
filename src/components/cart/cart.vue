59167207ff932d8ea26aff9a25dda89d297d3b12
jianqiao <tedqiao@gmail.com>
2018-04-07 23:18:10 -0400

    finalize cart
    finalize cart

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -3,20 +3,24 @@
   <div class="content">
     <div class="content-left">
       <div class="logo-wrapper">
-        <div class="logo">
-          <span class="icon-shopping_cart"></span>
+        <div class="logo" :class="{'highlignt':totalCount>0}">
+          <span class="icon-shopping_cart" :class="{'highlignt':totalCount>0}"></span>
+        </div>
+        <div v-show="totalCount>0" class="num">
+          {{totalCount}}
         </div>
       </div>
-      <div class="spend">
-        ¥ 0
+      <div class="payment" :class="{'highlignt':totalPrice>0}">
+        ¥ {{totalPrice}}
       </div>
       <div class="desc">
         另需配送费¥ {{deliveryPrice}}元
       </div>
     </div>
     <div class="content-right">
-      <div class="desc">
-        ¥ {{minPrice}}起送
+      <div class="pay"
+      :class="{'checkout':this.minPrice<=this.totalPrice}">
+        {{contentRdesc}}
       </div>
     </div>
   </div>
@@ -31,6 +35,38 @@ export default {
     },
     minPrice: {
       type: Number,
+    },
+    itemsInCart: {
+      type: Array,
+      default() {
+        return [{ price: 10, count: 1 }];
+      },
+    },
+  },
+  computed: {
+    totalCount() {
+      let count = 0;
+      this.itemsInCart.forEach((item) => {
+        count += item.count;
+      });
+      return count;
+    },
+    totalPrice() {
+      let price = 0;
+      this.itemsInCart.forEach((item) => {
+        price += item.price * item.count;
+      });
+      return price;
+    },
+    contentRdesc() {
+      if (this.totalPrice === 0) {
+        return `¥ ${this.minPrice}起送`;
+      }
+
+      if (this.totalPrice >= this.minPrice) {
+        return '去结账';
+      }
+      return `还差¥ ${this.minPrice - this.totalPrice}起送`;
     },
   },
 };
